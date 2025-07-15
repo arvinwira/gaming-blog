399e5e8ebc558e4dec4a114018a9b1c009f008d7
arvinwira <arvinwira@gmail.com>
2025-07-15 12:18:15 +0700

    sitemap

---
+++ app/sitemap.xml/route.js
@@ -0,0 +1,52 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+function getPosts() {
+  const postsDirectory = path.join(process.cwd(), 'posts');
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  return fileNames.map(fileName => {
+    const slug = fileName.replace(/\.md$/, '');
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+    const { data } = matter(fileContents);
+    return { slug, ...data };
+  });
+}
+
+export async function GET() {
+  const posts = getPosts();
+  const siteUrl = 'https://chronicreload.com';
+
+  const staticRoutes = [
+    { url: siteUrl, lastModified: new Date().toISOString() },
+    { url: `${siteUrl}/about`, lastModified: new Date().toISOString() },
+    { url: `${siteUrl}/contact`, lastModified: new Date().toISOString() },
+    { url: `${siteUrl}/categories`, lastModified: new Date().toISOString() },
+  ];
+
+  const postRoutes = posts.map(({ slug }) => ({
+    url: `${siteUrl}/blog/${slug}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  const allRoutes = [...staticRoutes, ...postRoutes];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${allRoutes
+  .map(route => `
+    <url>
+      <loc>${route.url}</loc>
+      <lastmod>${route.lastModified}</lastmod>
+    </url>`)
+  .join('')}
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
