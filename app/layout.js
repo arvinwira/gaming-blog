ae3d4ff6d9fea1eb07bf21ed426b88a215adf975
arvinwira <arvinwira@gmail.com>
2025-07-17 15:33:57 +0700

    favicon

--- app/layout.js
+++ app/layout.js
@@ -52,6 +52,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script
           async
