f662156538260d2255b585d34ea4ffd0e98b85e8
arvinwira <arvinwira@gmail.com>
2025-07-20 18:44:23 +0700

    google analytics

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import { ThemeProvider } from '@/components/ThemeProvider'
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script'
 
 
 const poppins = Poppins({
@@ -45,20 +46,34 @@ export default function RootLayout({ children }) {
         <link rel="apple-touch-icon" href="/favicon.ico" />
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <script
+        <Script
           async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8887590102646300"
+          src="https://pagead2.googlesyndication.com/pagead/js?client=ca-pub-8887590102646300"
           crossOrigin="anonymous"
         />
+
+        {/* GA4 */}
+        <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-YC0HP8ZFM9"
+        />
+        <Script id="ga4-init" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-YC0HP8ZFM9');
+        `}
+        </Script>
        
-        {/* ✅ Open Graph Tags */}
+        {/*Open Graph Tags */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://chronicreload.com/" />
         <meta property="og:title" content="Chronic Reload | Your Gaming News Hub" />
         <meta property="og:description" content="Stay up to date with the latest gaming news, reviews, and upcoming releases. Chronic Reload brings you curated content and fresh updates for gamers." />
         <meta property="og:image" content="https://chronicreload.com/logo.png" />
 
-        {/* ✅ Twitter Card Tags */}
+        {/* Twitter Card Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:url" content="https://chronicreload.com/" />
         <meta name="twitter:title" content="Chronic Reload | Your Gaming News Hub" />
