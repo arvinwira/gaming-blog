fcba13039783bc5b205393559cde72520287fc10
arvinwira <arvinwira@gmail.com>
2026-03-22 07:35:53 +0700

    added microsoft clarity

--- app/layout.js
+++ app/layout.js
@@ -86,6 +86,16 @@ export default function RootLayout({ children }) {
         gtag('config', 'G-YC0HP8ZFM9');
         `}
         </Script>
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vzhjz9wbvj");
+          `}
+        </Script>
       </body>
     </html>
   );
