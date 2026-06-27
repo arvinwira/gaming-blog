02ec35c173f9112ee99cd7222d5ad42a045b2944
arvinwira <arvinwira@gmail.com>
2026-06-27 23:38:52 +0700

    new post

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ import nextMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 const withMDX = nextMDX()
